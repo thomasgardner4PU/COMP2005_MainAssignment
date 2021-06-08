230de3b71548cee94483f60387e99776ef3453b8
tom <thomas.gardner-1@studnets.plymouth.ac.uk>
2021-06-08 20:47:46 +0100

    edited tests

--- Tests/UnitTests/CuisineneighbourhoodUnitTest.java
+++ Tests/UnitTests/CuisineneighbourhoodUnitTest.java
@@ -15,18 +15,18 @@ class CuisineneighbourhoodUnitTest {
     @Test
     public void CuisineNeighborhoodTests() {
         Restaurants wrongCuisine = new Restaurants("Manhattan", "American");
-        Restaurants wrongNeighborhod = new Restaurants("Brooklyn", "Asian");
+        Restaurants wrongNeighborhood = new Restaurants("Brooklyn", "Asian");
         Restaurants correctRestaurant = new Restaurants("Manhattan", "Asian");
 
         AllRestaurants allRestaurants = new AllRestaurants();
-        allRestaurants.restaurants = new Restaurants[]  { wrongCuisine, wrongNeighborhod, correctRestaurant };
+        allRestaurants.restaurants = new Restaurants[]  { wrongCuisine, wrongNeighborhood, correctRestaurant };
 
         Restaurants[] test = Cuisineneighbourhood.Filter("Asian", "Manhattan", allRestaurants);
 
         assertEquals(1,test.length); // only expecting the array to have item value inside
         assertEquals(correctRestaurant,test[0]); // make sure that only value inside of test array is the correct restaurant
         assertNotEquals(wrongCuisine, test[0]);
-        assertNotEquals(wrongNeighborhod, test[0]);
+        assertNotEquals(wrongNeighborhood, test[0]);
     }
 
 }
--- Tests/UnitTests/InspectionScoresUnitTests.java
+++ Tests/UnitTests/InspectionScoresUnitTests.java
@@ -1,13 +1,28 @@
 package UnitTests;
 
+import com.AllRestaurants;
+import com.Requirements.InspectionScores;
 import com.Restaurants;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class InspectionScoresUnitTests {
 
     @Test
     public void InspectionScoreTests() {
         Restaurants correctScoreRightNeighborhood = new Restaurants("13", "Brooklyn" );
-        Restaurants wrong
+        Restaurants wrongScoreRightNeighborhood = new Restaurants("20", "Brooklyn");
+        Restaurants wrongScoreWrongNeighborhood = new Restaurants("25", "Plymouth");
+
+        AllRestaurants allRestaurants = new AllRestaurants();
+        allRestaurants.restaurants = new Restaurants[] { correctScoreRightNeighborhood, wrongScoreRightNeighborhood, wrongScoreWrongNeighborhood};
+
+        Restaurants[] test = InspectionScores.getInspectionScores("Brooklyn", "10", allRestaurants).toArray(new Restaurants[0]);
+
+        assertEquals(1, test.length);
+        assertEquals(correctScoreRightNeighborhood, test[0]);
+        assertEquals(wrongScoreRightNeighborhood, test[0]);
+        assertEquals(wrongScoreWrongNeighborhood, test[0]);
     }
 }
